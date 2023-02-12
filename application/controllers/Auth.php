22efed69aae6991ad1e0d95a83e4fa0e370a01d0
Yosua Hutagalung <yosua.hutagalung05@gmail.com>
2023-02-12 19:18:58 +0700

    Add register page form validations

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,7 +7,7 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->library('form_validation')
+        $this->load->library('form_validation');
     }
 
     public function index()
@@ -20,9 +20,23 @@ class Auth extends CI_Controller
 
     public function register()
     {
-        $data['title'] = 'Register';
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/register');
-        $this->load->view('templates/auth_footer');
+        // Add form validations
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[8]|matches[password2]', [
+            'matches' => "Passwords don't match!",
+            'min_length' => "Password needs to be at least 8 characters."
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Register';
+            $this->load->view('templates/auth_header');
+            $this->load->view('auth/register');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo 'Data successfully added.';
+        }
     }
 }
--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -11,13 +11,16 @@
                         </div>
                         <form class="user" method="post" action="<?= base_url('auth/register') ?>">
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name">
+                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Full Name" value="<?= set_value('name'); ?>">
+                                <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
+                                <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group">
                                 <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                                <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group">
                                 <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
