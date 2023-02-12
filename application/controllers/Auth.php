55a99437e2b4eab2730960681eb52d85fbf99be7
Yosua Hutagalung <yosua.hutagalung05@gmail.com>
2023-02-12 18:55:00 +0700

    Add form validation to controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,6 +4,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Auth extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation')
+    }
+
     public function index()
     {
         $data['title'] = "Login";
