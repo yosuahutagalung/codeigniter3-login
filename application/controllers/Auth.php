34e2f7fef779f1d3a06febb6c927caa51d80074d
Yosua Hutagalung <yosua.hutagalung05@gmail.com>
2023-02-12 19:33:00 +0700

    Finish implementation of register form

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -22,7 +22,9 @@ class Auth extends CI_Controller
     {
         // Add form validations
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already been registered!'
+        ]);
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
         $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[8]|matches[password2]', [
             'matches' => "Passwords don't match!",
@@ -36,7 +38,21 @@ class Auth extends CI_Controller
             $this->load->view('auth/register');
             $this->load->view('templates/auth_footer');
         } else {
-            echo 'Data successfully added.';
+            htmlspecialchars($this->input->post('name', true));
+            htmlspecialchars($this->input->post('email', true));
+
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            redirect('auth');
         }
     }
 }
